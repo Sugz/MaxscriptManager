7dfb506b82213f5267aaa218cf6388b4cb3a7e79
Sugz <plantec.clement@gmail.com>
2017-08-30 00:29:02 +0200

    Update MaxscriptManager

--- MaxscriptManager/Source/MParser.cs
+++ MaxscriptManager/Source/MParser.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,9 +18,73 @@ namespace MaxscriptManager.Src
     public static class MParser
     {
 
+        public static IEnumerable<Run> FormatFunctionName(string name)
+        {
+            foreach (string str in Regex.Split(name, @"(?<=[ :])"))
+            {
+                Run run = new Run(str);
+                if (str.EndsWith(":"))
+                {
+                    run.Foreground = new SolidColorBrush(Color.FromArgb(255, 185, 185, 185));
+                    run.FontStyle = FontStyles.Italic;
+                }
+                yield return run;
+            }
+        }
+
+        private static void FormatStandardCode(string str, ref Paragraph paragraph)
+        {
+            foreach(string split in str.SplitAndKeep("\t ".ToCharArray()))
+            {
+                foreach (string matches in Regex.Matches(split, "[0-9.]+|[^0-9.]+").Cast<Match>().Select(match => match.Value))
+                {
+                    foreach (string s in Regex.Split(matches, @"(?<=[ :])"))
+                    {
+                        Run run = new Run(s);
+                        if (float.TryParse(s, out float result))
+                            run.Foreground = Resource<SolidColorBrush>.GetColor("OrangeText");
+                        else if (s.EndsWith(":"))
+                        {
+                            run.Foreground = new SolidColorBrush(Color.FromArgb(255, 185, 185, 185));
+                            run.FontStyle = FontStyles.Italic;
+                        }
+                        else if (s.StartsWith("#"))
+                            run.Foreground = Resource<SolidColorBrush>.GetColor("PinkText");
+                        else if (MConstants.ParserColors["ControlColor"].Contains(s.ToLower()))
+                            run.Foreground = Resource<SolidColorBrush>.GetColor("ControlColor");
+                        else if (MConstants.ClassDef.Contains(s.ToLower()) || MConstants.ParserColors["BlueText"].Contains(s.ToLower()))
+                            run.Foreground = Resource<SolidColorBrush>.GetColor("BlueText");
+                        else if (MConstants.ParserColors["LightBlueText"].Contains(s.ToLower()))
+                            run.Foreground = Resource<SolidColorBrush>.GetColor("LightBlueText");
+                        else if (MConstants.ParserColors["DarkBlueText"].Contains(s.ToLower()))
+                            run.Foreground = Resource<SolidColorBrush>.GetColor("DarkBlueText");
+                        else if (MConstants.ParserColors["VioletText"].Contains(s.ToLower()))
+                            run.Foreground = Resource<SolidColorBrush>.GetColor("VioletText");
+                        else if (MConstants.ParserColors["LightRedText"].Contains(s.ToLower()))
+                            run.Foreground = Resource<SolidColorBrush>.GetColor("LightRedText");
+                        else if (MConstants.ParserColors["GoldText"].Contains(s.ToLower()))
+                            run.Foreground = Resource<SolidColorBrush>.GetColor("GoldText");
+                        else if (MConstants.ParserColors["YellowText"].Contains(s.ToLower()))
+                            run.Foreground = Resource<SolidColorBrush>.GetColor("YellowText");
+                        else if (MConstants.ParserColors["LightGreenText"].Contains(s.ToLower()))
+                            run.Foreground = Resource<SolidColorBrush>.GetColor("LightGreenText");
+                        else if (MConstants.ParserColors["DarkGreenText"].Contains(s.ToLower()))
+                            run.Foreground = Resource<SolidColorBrush>.GetColor("DarkGreenText");
+
+                        paragraph.Inlines.Add(run);
+                    }
+                }
+            }
+        }
+
         private static Run FormatStandardCode(string s)
         {
             Run run = new Run(s);
+
+            //string containing numnber
+            
+            string[] output = Regex.Matches(s, "[0-9.]+|[^0-9.]+").Cast<Match>().Select(match => match.Value).ToArray();
+
             if (MConstants.ClassDef.Concat(MConstants.BlueWords).Contains(s.ToLower()))
                 run.Foreground = Resource<SolidColorBrush>.GetColor("BlueText");
             else if (MConstants.LightBlueWords.Contains(s.ToLower()))
@@ -40,10 +105,10 @@ namespace MaxscriptManager.Src
         }
 
 
-        private static TextBlock FormatCommentary(string s)
+        private static Run FormatCommentary(string s)
         {
-            TextBlock run = new TextBlock() { Text = s, TextWrapping = TextWrapping.NoWrap };
-            run.Foreground = Resource<SolidColorBrush>.GetColor("MaxGreen");
+            Run run = new Run(s);
+            run.Foreground = Resource<SolidColorBrush>.GetColor("CommentaryColor");
             run.FontStyle = FontStyles.Italic;
             return run;
         }
@@ -61,10 +126,21 @@ namespace MaxscriptManager.Src
                 if (delims.Any(x => line.Contains(x)) || isString || isCommentary)
                 {
                     bool singleLine = false;
+
+                    // string has priorities over commentaries but not if a commentary has already begun
                     foreach (string str in line.SplitAndKeep(delims))
                     {
+                        // Begining or end of string
+                        if (str == "\"" && !isCommentary)
+                        {
+                            isString = !isString;
+                            paragraph.Inlines.Add(FormatString(str));
+                        }
+                        // Currently in string
+                        else if (isString && !isCommentary)
+                            paragraph.Inlines.Add(FormatString(str));
                         // Begining or end of commentary
-                        if (Array.FindIndex(MConstants.ComDelimiters, x => str == x) is int comIndex && comIndex != -1)
+                        else if (Array.FindIndex(MConstants.ComDelimiters, x => str == x) is int comIndex && comIndex != -1)
                         {
                             isCommentary = (comIndex != 2);
                             singleLine = (comIndex == 0);
@@ -73,25 +149,21 @@ namespace MaxscriptManager.Src
                         // Currently in commentary
                         else if (isCommentary)
                             paragraph.Inlines.Add(FormatCommentary(str));
-                        // Begining or end of string
-                        else if (str == "\"")
-                        {
-                            isString = !isString;
-                            paragraph.Inlines.Add(FormatString(str));
-                        }
-                        // Currently in string
-                        else if (isString)
-                            paragraph.Inlines.Add(FormatString(str));
                         // Standard code
                         else
-                            str.SplitAndKeep("\t ".ToCharArray()).ForEach(x => paragraph.Inlines.Add(FormatStandardCode(x)));
+                        {
+                            FormatStandardCode(str, ref paragraph);
+                            //str.SplitAndKeep("\t ".ToCharArray()).ForEach(x => paragraph.Inlines.Add(FormatStandardCode(x)));
+                        }
+                            
                     }
                     if (singleLine)
                         isCommentary = false;
                 }
                 // Standard code
                 else
-                    line.SplitAndKeep("\t ".ToCharArray()).ForEach(x => paragraph.Inlines.Add(FormatStandardCode(x)));
+                    FormatStandardCode(line, ref paragraph);
+                    //line.SplitAndKeep("\t ".ToCharArray()).ForEach(x => paragraph.Inlines.Add(FormatStandardCode(x)));
 
                 document.Blocks.Add(paragraph);
             }
